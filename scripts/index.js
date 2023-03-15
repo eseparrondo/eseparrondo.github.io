11188b661bcc892d2d2c472ff40f8bdba5a2a0c6
parr0 <127327035+parr0@users.noreply.github.com>
2023-03-15 18:01:51 +0100

    changeBg
    separate files for html, css and js
    divided in directories
    css directory also contains a fonts folder with matrix font
    background updated with matrix style

---
+++ scripts/index.js
@@ -0,0 +1,82 @@
+var json = [];
+var pala  = [];
+var defi = [];
+var lon;
+
+$.ajax({
+  url: "https://raw.githubusercontent.com/ssvivian/WebstersDictionary/master/dictionary.json",
+  async: false,
+  cache: false,
+  dataType: "json",
+  processData: true,
+})
+.done(//obtencion de los arrays de palabras y definiciones para su posterior empleo
+  function(data){
+    lon = data.length;
+    //console.log(data);
+    for (let i = 0; i < data.length; i++) {
+            pala.push(data[i].word);  
+            defi.push(data[i].definitions);
+        }
+        
+  }
+)
+//console.log(pala);
+//console.log(defi);
+console.log(lon);
+//definicion del tope de numeros que se pueden generar aleatoriamente
+var tope = Math.floor(Math.random()*lon);
+  console.log(tope);
+
+  //recogemos el svg que va a hacer de fondo
+var fondo = document.getElementById('fondo');
+
+//creamos el array de los simbolos que van a simular la cascada
+var casca = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',];
+var dimensiones = fondo.getBoundingClientRect();
+var avance = [];
+function generateMatrix() {
+    const width = dimensiones.width;
+	const height = dimensiones.height;
+	for (let x = 0; x < width; x += 50) {
+    avance[x] = [];
+        console.log('generao');
+		for (let y = 0; y < height; y += 50) {
+			const char = casca[Math.floor(Math.random() * casca.length)];
+			const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+			text.setAttribute('x', x);
+			text.setAttribute('y', y);
+			text.setAttribute('fill', '#00ff2b');
+			text.textContent = char;
+			fondo.appendChild(text);
+            avance[x][y] = {
+                text,
+                timer: 0,
+            };
+		}
+	}
+}
+
+function updateMatrix() {
+    for (let x = 0; x < avance.length; x++) {
+        for (let y = 0; y < avance[x].length; y++) {
+            const char = casca[Math.floor(Math.random() * casca.length)];
+            
+            if (avance[x][y].timer === 0) {
+                avance[x][y].text.textContent = char;
+                avance[x][y].timer = Math.floor(Math.random() * 10) + 1;
+            } else {
+                avance[x][y].timer--;
+            }
+        }
+    }
+}
+
+setInterval(() => {
+	generateMatrix();
+}, 60);
+
+setInterval(() => {
+    document.getElementById("fondo").innerHTML = '';    
+    updateMatrix();
+}, 120);
